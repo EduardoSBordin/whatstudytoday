9eb8ff02f6b7b6c5f3ebb34b7beff5048ad2175f
Eduardo Bordin <dudubordin26@gmail.com>
2023-10-26 18:45:27 -0300

    v-0004

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 let getBtnModal = document.querySelector('#btnModal');
 let linksNavMobileModal = document.querySelector('#linksNavMobileModal');
 
+let displayJS = document.querySelector('#displayJS');
+let displayPhp = document.querySelector('#displayPhp');
+
 let count = 0;
 
 getBtnModal.addEventListener('click', () => {
@@ -18,3 +21,62 @@ getBtnModal.addEventListener('click', () => {
     getBtnModal.classList.toggle('active');
 
 })
+
+let getDate = new Date();
+
+let getDay = getDate.getDay();
+
+function getDayFunction(){
+
+
+    if(getDay == 0){
+    
+        console.log('Sunday');
+    
+        displayJS.style.display = "none";
+        displayPhp.style.display = "flex";
+    }
+    else if(getDay == 1){
+    
+        console.log('Monday');
+    
+        displayJS.style.display = "flex";
+        displayPhp.style.display = "none";
+    }
+    else if(getDay == 2){
+    
+        console.log('Tuesday');
+    
+        displayJS.style.display = "none";
+        displayPhp.style.display = "flex";
+    }
+    else if(getDay == 3){
+    
+        console.log('Wednsday');
+    
+        displayJS.style.display = "flex";
+        displayPhp.style.display = "none";
+    }
+    else if(getDay == 4){
+    
+        console.log('Thursday');
+    
+        displayJS.style.display = "none";
+        displayPhp.style.display = "flex";
+    }
+    else if(getDay == 5){
+    
+        console.log('Friday');
+    
+        displayJS.style.display = "flex";
+        displayPhp.style.display = "none";
+    }
+    else if(getDay == 6){
+    
+        console.log('Saturday');
+    
+        displayJS.style.display = "none";
+        displayPhp.style.display = "flex";
+    }
+}
+getDayFunction();
